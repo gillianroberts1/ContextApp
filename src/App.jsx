3d933ed8cefc84487b750d8931b65178a0346b57
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-10-21 14:16:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import BookList from "./components/BookList";
+import NavBar from "./components/NavBar";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BookList.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from "react";
+
+class BookList extends Component {
+    render() { 
+        return (
+            <div className='book-list'>
+                <ul>
+                    <li>The Way of Kings</li>
+                    <li>The Name of the Wind</li>
+                    <li>The Final Empire</li>
+
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default BookList;
