f7ae3ebab2922fc02c3baed8ee3b9c538863035c
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-10-22 22:09:52 +0100

    booklist changed to functional comp to allow useContext to bs used

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,26 +1,48 @@
 // eslint-disable-next-line no-unused-vars
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-    static contextType = ThemeContext
+// class BookList extends Component {
+//   static contextType = ThemeContext;
 
-    render() { 
-        console.log(this.context);
-        const { isLightTheme, light, dark } = this.context
-        const theme = isLightTheme ? light : dark
-        
-        return (
-            <div className='book-list' style= {{ color: theme.syntax, background: theme.bg }}>
-                <ul>
-                    <li style= {{ background: theme.ui }}>The Way of Kings</li>
-                    <li style= {{ background: theme.ui }}>The Name of the Wind</li>
-                    <li style= {{ background: theme.ui }}>The Final Empire</li>
+//   render() {
+//     console.log(this.context);
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
 
-                </ul>
-            </div>
-        );
-    }
-}
- 
+//     return (
+//       <div
+//         className="book-list"
+//         style={{ color: theme.syntax, background: theme.bg }}
+//       >
+//         <ul>
+//           <li style={{ background: theme.ui }}>The Way of Kings</li>
+//           <li style={{ background: theme.ui }}>The Name of the Wind</li>
+//           <li style={{ background: theme.ui }}>The Final Empire</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// changed go functional component so that useContext can be used
+
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>The Way of Kings</li>
+        <li style={{ background: theme.ui }}>The Name of the Wind</li>
+        <li style={{ background: theme.ui }}>The Final Empire</li>
+      </ul>
+    </div>
+  );
+};
+
 export default BookList;
